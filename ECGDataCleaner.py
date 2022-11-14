30a21184987f84dafc062b8eafb2ef14ab63e3ec
Aleksa V <viganjg12@gmail.com>
2022-11-13 19:36:44 -0500

    Working

--- ECGDataCleaner.py
+++ ECGDataCleaner.py
@@ -6,12 +6,12 @@ from ECGgatherData import ECGGatherer
 from EasyMLLib.helper import Helper
 
 DIB_NAME = "DIB2"
-DATASETS_PATH = path.join("data", DIB_NAME, 'dataRaw', 'ID') #possibly wrong rn
+DATASETS_PATH = path.join("data", DIB_NAME, 'dataRaw') #possibly wrong rn
 SELECTED_DATA_SET_PATH = path.join("Single", "ET")
 CONCAT_FILE_PATH = path.join("data", DIB_NAME)
 CONCAT_FILE_NAME = "concat-data"
 CONCAT_FILE_EXT = ".csv"
-CONCAT_FULLPATH_WITHOUT_EXT = path.join(CONCAT_FILE_PATH, CONCAT_FILE_NAME) #data data/DIB2/-data
+CONCAT_FULLPATH_WITHOUT_EXT = path.join(CONCAT_FILE_PATH, CONCAT_FILE_NAME) #data data/DIB2/concat-data
 
 class ECGDataCleaner:
 
@@ -49,81 +49,6 @@ class ECGDataCleaner:
             return pd.DataFrame(pd.read_csv(CONCAT_FULL_PATH_WITH_EXT))
 
     def cleanData(self, data: pd.DataFrame, pathStr: str) -> pd.DataFrame:
-        # Dropping these rows:
-        # fpogs (start time since init of pog)
-        # fpogid (id of the gaze.. which is duplicate info since fpogd shows duration)
-        # CS (cursor position id)
-        # CX and CY (Cursor POS)
-
-        # data.drop(['FPOGS', 'FPOGID', 'CS', 'CX', 'CY'], axis=1, inplace=True)
-        data.drop(['FPOGS', 'FPOGID', 'CS', 'CX', 'CY', 'BKID',
-                  'BKDUR', 'BKPMIN'], axis=1, inplace=True)
-
-        # ANALYZE:
-        # fpogv (is the fixation pog valid)
-        # (The valid flag with value of 1 (TRUE) if the fixation POG data is valid, and 0
-        # (FALSE) if it is not. FPOGV valid is TRUE ONLY when either one, or both, of the
-        # eyes are detected AND a fixation is detected. FPOGV is FALSE all other times, for
-        # example when the subject blinks, when there is no face in the field of view,
-        # when the eyes move to the next fixation (i.e. a saccade). )
-        # when fpogx = 0, the row is dropped
-        # -- Decided to leave it for now since rapid eye movements may be useful (saccade)
-
-        # LPCX LPCY  (left eye pupil x and y  coords in cam img as fraction of cam img size)
-        # RPCX RPCY  (right eye pupil x and y  coords in cam img as fraction of cam img size)
-
-        # LPD/RPD (diameter of left/right eye pupil in pixels)
-
-        # LPS/RPS (: The scale factor of the left/right eye pupil (unitless). Value equals 1 at calibration
-        # depth, is less than 1 when user is closer to the eye tracker and greater than 1 when user
-        # is further away.)
-
-        # LPV/RPV (whether the left/right eye pupil info is correct
-
-        # Correct:
-        # (bkdur is the preceding blink duration)
-        # bkid should be changed to either 0 for none or 1 for blinking
-
-        # instead of returning an id, it returns 0 for no blink and 1 for a blink right now
-        # data['BKID'] = np.where(data["BKID"] > 0, 1, 0)
-        # REMOVED
-
-        # # Calculating number of invalid rows -> [removed now]
-        # print("Number of invalid rows FPOGV:")
-        # print(data.groupby(by='FPOGV').count())
-        # print("Number of invalid rows LPV:")
-        # print(data.groupby(by='LPV').count())
-        # print("Number of invalid rows RPV:")
-        # print(data.groupby(by='RPV').count())
-        # print("Number of invalid rows BPOGV:")
-        # print(data.groupby(by='BPOGV').count())
-
-        # # Dropping the rows with invalid data
-        # data.drop(data[data['FPOGV'] < 1].index, inplace=True)
-        # data.drop(data[data['LPV'] < 1].index, inplace=True)
-        # data.drop(data[data['RPV'] < 1].index, inplace=True)
-        # data.drop(data[data['BPOGV'] < 1].index, inplace=True)
-
-        # # Calculating number of invalid rows -> [removed now]
-        # print("Number of invalid rows FPOGV:")
-        # print(data.groupby(by='FPOGV').count())
-        # print("Number of invalid rows LPV:")
-        # print(data.groupby(by='LPV').count())
-        # print("Number of invalid rows RPV:")
-        # print(data.groupby(by='RPV').count())
-        # print("Number of invalid rows BPOGV:")
-        # print(data.groupby(by='BPOGV').count())
-
-        # Dropping the columns that determined invalid data (not needed anymore)
-        data.drop(['FPOGV', 'LPV', 'RPV', 'BPOGV'], axis=1, inplace=True)
-
-        # COMBINING INFO
-        data['LPDLPS'] = np.where(True, data['LPD']*data['LPS'], 0)
-        data['RPDRPS'] = np.where(True, data['RPD']*data['RPS'], 0)
-
-        # Dropping unused columns
-        data.drop(['LPD', 'LPS', 'RPD', 'RPS'], axis=1, inplace=True)
-
         print(f"Saving {pathStr}")
         data.to_csv(pathStr, index=False)
         return data
--- ECGgatherData.py
+++ ECGgatherData.py
@@ -1,59 +1,75 @@
 import os.path as path
+import string
 from EasyMLLib.helper import Helper
 from EasyMLLib.DataGatherer import DataGatherer
 
 import pandas as pd
 
-DIB_NAME = "DIB1"
-DATASETS_PATH = path.join("data", DIB_NAME, 'dataRaw', 'ID')
-SELECTED_DATA_SET_PATH = path.join("Single", "ET")
+DIB_NAME = "DIB2"
+DATASETS_PATH = path.join("data", DIB_NAME, 'dataRaw') #possibly wrong rn
+SELECTED_DATA_SET_PATH = path.join("ECG")
 CONCAT_FILE_PATH = path.join("data", DIB_NAME)
 CONCAT_FILE_NAME = "concat-data"
 CONCAT_FILE_EXT = ".csv"
-CONCAT_FULLPATH_WITHOUT_EXT = path.join(CONCAT_FILE_PATH, CONCAT_FILE_NAME)
+CONCAT_FULLPATH_WITHOUT_EXT = path.join(CONCAT_FILE_PATH, CONCAT_FILE_NAME) #data data/DIB2/concat-data
 
-CLASSIFIERS = [0, 1, 2]
+CLASSIFIERS = [0, 2]
 
 MIN_ID = 1
-MAX_ID = 28
+MAX_ID = 16
 
 class ECGGatherer:
     def main(self):
         self.gatherDataConcat()
 
-    # # Gathers the data for a given id
-    # def gatherData(self, idNum) -> pd.DataFrame:
-    #     CONCAT_FULL_PATH_WITH_EXT = CONCAT_FULLPATH_WITHOUT_EXT + \
-    #         "-" + str(idNum) + CONCAT_FILE_EXT
+    def getProperIdNumber(self, idNum) -> string: 
+        if (idNum >= 10):
+            return f'{idNum}'
+        else:
+            return f'0{idNum}'
 
-    #     # Function called when the file isn't found
-    #     def concatFunc() -> pd.DataFrame:
-    #         id = f"0{idNum}"
-    #         dfs = []
-    #         for classifier in CLASSIFIERS:
-    #             filePath = path.join(
-    #                 DATASETS_PATH + f'{idNum}'.zfill(2), SELECTED_DATA_SET_PATH, f'ID{id}_ET_{classifier}.xlsx')
-    #             print(filePath)
-    #             tmpDf: pd.DataFrame = pd.DataFrame(pd.read_excel(filePath), columns=['FPOGX', 'FPOGY', 'FPOGS', 'FPOGD', 'FPOGID', 'FPOGV', 'BPOGX', 'BPOGY',
-    #                                                'BPOGV', 'CX', 'CY', 'CS', 'LPCX', 'LPCY', 'LPD', 'LPS', 'LPV', 'RPCX', 'RPCY', 'RPD', 'RPS', 'RPV', 'BKID', 'BKDUR', 'BKPMIN'])
-    #             tmpDf['classifier'] = classifier
-    #             Helper.quickDfStat(tmpDf)
-    #             dfs.append(tmpDf)
-    #         finalDf = pd.concat(dfs)
-    #         print("Concatenated!")
-    #         Helper.quickDfStat(finalDf)
-    #         finalDf.to_csv(CONCAT_FULL_PATH_WITH_EXT, index=False)
-    #         print(f"Saved: {CONCAT_FULL_PATH_WITH_EXT}")
-    #         return finalDf
 
-    #     df = DataGatherer().gatherDataFromFile(
-    #         CONCAT_FULL_PATH_WITH_EXT, concatFunc=concatFunc)
-    #     # Helper.quickDfStat(df)
-    #     return df
+    # Gathers the data for a given id
+    def gatherData(self, idNum) -> pd.DataFrame:
+        CONCAT_FULL_PATH_WITH_EXT = CONCAT_FULLPATH_WITHOUT_EXT + \
+            "-" + str(idNum) + CONCAT_FILE_EXT #data/DIB2/concat-data-2.csv
+
+        # Function called when the file isn't found
+        def concatFunc() -> pd.DataFrame:
+            id = f"0{idNum}"
+            dfs = []
+            for classifier in CLASSIFIERS:
+
+                # hardcoded case to skip 11 because they dont have 0 back
+                if (idNum == 11 and classifier == 0):
+                    continue
+
+                idNumberCleaned = self.getProperIdNumber(idNum)
+
+                filePath = path.join(
+                    DATASETS_PATH , f'ID{idNumberCleaned}', SELECTED_DATA_SET_PATH, f'ID{idNumberCleaned}_ECG_{classifier}Back.csv') #ID01/ECG/ID01_ECG_0Back.csv
+                print(filePath)
+                tmpDf: pd.DataFrame = pd.DataFrame(pd.read_csv(filePath, usecols =[0,1], names=['time', 'SP']))
+                
+                tmpDf['classifier'] = classifier
+                #Helper.quickDfStat(tmpDf)
+                dfs.append(tmpDf)
+            
+            finalDf = pd.concat(dfs)
+            print("Concatenated!")
+            #Helper.quickDfStat(finalDf)
+            finalDf.to_csv(CONCAT_FULL_PATH_WITH_EXT, index=False)
+            print(f"Saved: {CONCAT_FULL_PATH_WITH_EXT}")
+            return finalDf
+
+        df = DataGatherer().gatherDataFromFile(
+            CONCAT_FULL_PATH_WITH_EXT, concatFunc=concatFunc)
+        # Helper.quickDfStat(df)
+        return df
 
     def gatherDataConcat(self) -> pd.DataFrame:
         CONCAT_FINAL_FULL_PATH_WITH_EXT = CONCAT_FULLPATH_WITHOUT_EXT + \
-            "-concat" + CONCAT_FILE_EXT
+            "-concat" + CONCAT_FILE_EXT #data/DIB2/concat-data-concat.csv
 
         # Function called when the file isn't found
         def concatFunc() -> pd.DataFrame:
